84ec75c3e3b3d60d0f4f50822fd834c103d619a5
teresanesteby <teresa.nesteby@gmail.com>
2015-11-18 14:47:50 -0800

    add/delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  #destroy comments if post is deleted from the db as well
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
